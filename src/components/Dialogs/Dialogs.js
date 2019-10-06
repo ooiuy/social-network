13d3025fb014188d317100bf5aad14bdfa354f5f
ooiuy <poisonooiuy@gmail.com>
2019-10-06 21:07:13 +0500

    add map dialog

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -11,42 +11,40 @@ const Dialogitem = props => {
   );
 };
 
-const Messages = (props)=>{
-    return(
-        <div className={classes.message}>{props.message}</div>
-    )
-}
+const Messages = props => {
+  return <div className={classes.message}>{props.message}</div>;
+};
 
 let dialogData = [
-  {id:1, name: 'Mars'},
-  {id:2, name: 'Maks'},
-  {id:3, name: 'Ruslan'},
-  {id:4, name: 'Artem'},
-  {id:5, name: 'Andrey'},
-  {id:6, name: 'Vlad-avtomat'}
-]
+  { id: 1, name: "Mars" },
+  { id: 2, name: "Maks" },
+  { id: 3, name: "Ruslan" },
+  { id: 4, name: "Artem" },
+  { id: 5, name: "Andrey" },
+  { id: 6, name: "Vlad-avtomat" }
+];
 let messageData = [
-  {id:1, message: 'krasav4ik'},
-  {id:2,  message: 'loh'},
-  {id:3,  message: 'vodila'},
-  {id:4,  message: 'povar'},
-  {id:5,  message: 'kartavii'},
-  {id:6,  message: 'ubia'}
-]
+  { id: 1, message: "krasav4ik" },
+  { id: 2, message: "loh" },
+  { id: 3, message: "vodila" },
+  { id: 4, message: "povar" },
+  { id: 5, message: "kartavii" },
+  { id: 6, message: "ubia" }
+];
+
+let messagesElements = messageData.map((m)=>{
+  return <Messages message={m.message} id={m.id} /> 
+})
+
+let dialogsElements = dialogData.map((d)=>{
+  return  <Dialogitem name={d.name} id={d.id} />
+}) 
 
 const Dialogs = () => {
   return (
     <div className={classes.dialogs}>
-      <div className={classes.dialogsItem}>
-        <Dialogitem name={dialogData[0].name} id={dialogData[0].id} />
-        <Dialogitem name={dialogData[1].name} id={dialogData[1].id} />
-        <Dialogitem name={dialogData[2].name} id={dialogData[2].id} />
-        <Dialogitem name={dialogData[3].name} id={dialogData[3].id} />
-        <Dialogitem name={dialogData[4].name} id={dialogData[4].id} />
-        <Dialogitem name={dialogData[5].name} id={dialogData[5].id} />
-      </div>
-      <div className={classes.messages}>
-        <Messages message={messageData[0].message} id={dialogData[0].id} />
+      <div className={classes.dialogsItem}>{dialogsElements}</div>
+      <div className={classes.messages}>{messagesElements}
       </div>
     </div>
   );
