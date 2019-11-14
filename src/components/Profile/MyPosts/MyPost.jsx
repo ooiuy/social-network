cb643fb6e397a15dccdeb13461068c771aca6116
mars <poisonooiuy@gmail.com>
2019-11-14 16:16:35 +0500

    added and fixed mypost

--- src/components/Profile/MyPosts/MyPost.jsx
+++ src/components/Profile/MyPosts/MyPost.jsx
@@ -3,13 +3,19 @@ import classes from "./MyPost.module.css";
 import Post from "./Post/Post";
 
 const MyPost = () => {
+
+  let PostData = [
+    {id: 1, message: 'Salamandra'},
+    {id: 2, message: 'Bratishka ejje'},
+    {id: 3, message: 'Давай, не ленись, делай'}
+  ]
+  let messageElements = PostData.map(m =>{
+    return <Post message={m.message} id={m.id} />
+  }) 
   return (
     <div className={classes.postBlock}>
-      POSTS123123
-      <div className={classes.postBlock}>post1</div>
-      <Post message="Salamandra" />
-      <Post message="bratishka" />
-      <Post message="Давай, не ленись,делай" />
+      POSTS
+      <div className={classes.postBlock}>{messageElements}</div>
     </div>
   );
 };
