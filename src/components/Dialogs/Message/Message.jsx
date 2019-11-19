0c85b81760de098c0e0f03c7541ed23bdbba7920
mars <poisonooiuy@gmail.com>
2019-11-19 11:36:16 +0500

    added onclick,ref,VD

--- src/components/Dialogs/Message/Message.jsx
+++ src/components/Dialogs/Message/Message.jsx
@@ -3,7 +3,20 @@ import classes from "./../Dialogs.module.css";
 
 
 const Messages = props => {
-  return <div className={classes.message}>{props.message}</div>;
+  let sendMessage = React.createRef()
+
+  let addMessage = (() => {
+    let text = sendMessage.current.value
+    alert(text)
+  })
+
+  return <div className={classes.message}>
+    <div>{props.message}</div>
+    <div><textarea ref={sendMessage}></textarea></div>
+    <div>
+      <button onClick={addMessage}>Отправить сообщение</button>
+    </div>
+  </div>;
 };
 
 
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -2,14 +2,20 @@ import React from 'react'
 import classes from './Post.module.css'
 
 const Post = (props) =>{
-    
+
+    let newPostElement = React.createRef()
+
+    let addPost = () => {
+        let text = newPostElement.current.value
+        alert(text)
+    }
     return(
         <div className={classes.item}>{props.message}
         <div className={classes.addtext}>
-            <textarea></textarea>
+            <textarea ref={newPostElement}></textarea>
         </div>
         <div className={classes.btnpost}>
-            <button>Опубликовать</button>
+            <button onClick={addPost}>Опубликовать</button>
         </div>
         </div>
     )
